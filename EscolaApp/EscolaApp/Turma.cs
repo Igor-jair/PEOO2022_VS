2477d0b0b15dfbbe5e1946fe7e8bbf77cb228b82
Igor-jair <105497607+Igor-jair@users.noreply.github.com>
2022-12-30 17:36:14 -0300

    d

--- EscolaApp/EscolaApp/Turma.cs
+++ EscolaApp/EscolaApp/Turma.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace EscolaApp
 {
-    class Turma
+    public class Turma
     {
         public int Id { get; set; }
         public string Curso { get; set; }
@@ -14,7 +14,7 @@ namespace EscolaApp
         public int AnoLetivo { get; set; }
         public override string ToString()
         {
-            return $"{Id} - {Curso} - {Descricao} - {AnoLetivo}"; 
+            return $"{Id} - {Curso} - {Descricao} - {AnoLetivo}";
         }
     }
 }
